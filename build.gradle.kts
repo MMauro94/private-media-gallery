66f150a3238873a9cd19bb72e54866ae02999ce5
Mauro Molin <molin.mauro@gmail.com>
2023-06-17 14:34:06 +0100

    Add JDK cryptographic provider

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,8 @@ kotlin {
                 implementation("io.ktor:ktor-server-netty:2.0.2")
                 implementation("io.ktor:ktor-server-html-builder-jvm:2.0.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+
+                implementation("dev.whyoleg.cryptography:cryptography-jdk:0.1.0")
                 implementation("app.cash.sqldelight:sqlite-driver:2.0.0-rc01")
             }
         }
